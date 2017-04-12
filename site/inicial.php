d2fc4f55009f96b1960acdc57dda5ac8a0da93b4
Cristiano Coelho <ccoelho.me@gmail.com>
2017-04-12 16:20:13 -0300

    site

---
+++ site/inicial.php
@@ -0,0 +1,44 @@
+  <!DOCTYPE html> 
+  <html lang="en">
+  <head>
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilos.css">  
+    <title>UpClass - início</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta charset="UTF-8"> 
+  </head> 
+  <body> 
+    <!-- Barra de navegação--> <nav class="navbar navbar-default navbar-fixed-top"> 
+    <div class="container-fluid"> 
+      <div class="navbar- header"> 
+        <a class="navbar-brand" href="www.upclass.com">UpClass</a> 
+      </div>
+      <ul class="nav navbar-nav">
+        <li class="active">
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a class="glyphicon glyphicon-user" href="#"></a>
+        </li>
+        <li>
+          <a href="#">Page 2</a>
+        </li>
+        <li>
+          <a href="#">Page 3</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div class="row">
+    <!-- barra lateral esquerda -->
+    <div class="col-sm-2">
+      <aside mobile-nofixed> 
+        <img id="foto_user" src="" alt="foto do usuário" width="128" height="128">
+      </aside>
+    </div>
+<div class="col-sm-7"><h2>Postar material</h2>
+<h4>Deixe aqui a sua contribuição para outros professores!</h4>  
+</div>
+    </div>
+  </body>
+  </html>
