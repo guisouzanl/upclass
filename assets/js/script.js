79ce0b54576b24a4ed20989c067e7ea9f1dc2d55
Cristiano Coelho <ccoelho.me@gmail.com>
2017-05-25 13:12:54 -0300

    js do authbox show login%hide register

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,78 @@
+"use strict";
+
 function addFriend(id, obj) {
 
-	if(id != '') {
+    if (id != '') {
 
-		$(obj).closest('.sugestaoitem').fadeOut();
+        $(obj).closest('.sugestaoitem').fadeOut();
 
-		$.ajax({
-			type:'POST',
-			url:'ajax/add_friend',
-			data:{id:id}
-		});
+        $.ajax({
+            type: 'POST',
+            url: 'ajax/add_friend',
+            data: { id: id }
+        });
 
-	}
-
-}
-	
-	function aceitarFriend(id, obj){
-
-			if(id != '') 
-	{
-		$(obj).closest('.requisicaoitem').fadeOut();
-
-		$.ajax({
-			type:'POST',
-			url:'ajax/aceitar_friend',
-			data:{id:id}
-		});
-	}
-
-}
-	function openAuth(authForm, elmnt, color) {
-    // Hide all elements with class="tabcontent" by default */
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
     }
 
-    // Remove the background color of all tablinks/buttons
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
-    }
-
-    // Show the specific tab content
-    document.getElementById(authForm).style.display = "block";
-
-    // Add the specific color to the button used to open the tab content
-    elmnt.style.backgroundColor = color;
 }
 
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+function aceitarFriend(id, obj) {
+
+    if (id != '') {
+        $(obj).closest('.requisicaoitem').fadeOut();
+
+        $.ajax({
+            type: 'POST',
+            url: 'ajax/aceitar_friend',
+            data: { id: id }
+        });
+    }
+
+}
+
+/* modelo de slide show do W3Schools - deve ser trocado pela lightbox? */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+// script do authbox /
+function mostrarElemento(idElemento) {
+    document.getElementById(idElemento).style.display = "block";
+}
+function ocultarElemento(idElemento) {
+    document.getElementById(idElemento).style.display = "none";
+}
+function validaLogin() {
+    var nome = document.getElementById("login").value;
+    var senha = document.getElementById("password").value;
+
+    if ((nome == "") && (senha == "")) {
+        alert("Tente novamente");
+    } else {
+        mostrarElemento("div_mensagem");
+        mostrarElemento("idioma")
+        ocultarElemento("div_login");
+    }
+}
