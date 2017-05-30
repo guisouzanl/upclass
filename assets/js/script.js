8f4cfd7a9c6e44651dfcc01875668c9d26bd0279
Cristiano Coelho <ccoelho.me@gmail.com>
2017-05-30 09:21:16 -0300

    reorganização dos arquivos

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,37 +34,6 @@ function aceitarFriend(id, obj) {
 
 }
 
-/* modelo de slide show do W3Schools - deve ser trocado pela lightbox? */
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
 // script do authbox /
 function mostrarElemento(idElemento) {
     document.getElementById(idElemento).style.display = "block";
